ea56d9303377ba6f839d7102761c5e01855e4700
Antony Perez <antonyperez0@yahoo.com>
2024-10-29 14:33:45 -0700

    did the movie and music classes

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,25 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Movie class:
+class Movie extends Media {
+  constructor(title, year, genre, director, duration, rating) {
+    super(title, year, genre);
+    this.director = director;
+    this.duration = duration;
+    this.rating = rating;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static longestMovie(movies) {
+    return movies.reduce(
+      (longest, movie) => (movie.duration > longest.duration ? movie : longest),
+      movies[0]
+    );
+  }
+}
 
 // don't change below
 module.exports = Movie;
